00b11fcc5bc904fbc7e8f8ae532bab9034f46935
Richard Baxter <richard.jonathan.baxter@gmail.com>
2009-07-23 14:02:46 +0200

    edited run.sh file to use new file system

--- linux_files/run.sh
+++ linux_files/run.sh
@@ -1,1 +1,3 @@
-LD_LIBRARY_PATH="`pwd`/jogl-1.1.1-linux-i586/lib/" CLASSPATH="`pwd`/jogl-1.1.1-linux-i586/lib/jogl.jar":"`pwd`/jogl-1.1.1-linux-i586/lib/gluegen-rt.jar":"`pwd`/src/game/game.jar":"`pwd`/src/" java Runner
+LD_LIBRARY_PATH="`pwd`/libs/jogl-1.1.1-linux-i586/lib/" 
+CLASSPATH="`pwd`/libs/jogl-1.1.1-linux-i586/lib/jogl.jar":"`pwd`/libs/jogl-1.1.1-linux-i586/lib/gluegen-rt.jar":"`pwd`/jar/2DGame_1.2.jar":"`pwd`/src/" 
+java Runner
