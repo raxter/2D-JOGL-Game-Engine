52d5a0ad031f5a723c2c13c492a2824194c01ca6
Richard Baxter <richard@richard-desktop.(none)>
2009-07-23 03:22:07 +0200

    all finished, i hope

--- create_API.sh
+++ create_API.sh
@@ -1,30 +1,33 @@
 # cleaning up dev folder
-rm -rf $1"_linux/"/
-rm -rf $1"_windows/"/
+
+rm -rf output/
+mkdir output/
+#rm -rf $1"_linux/"/
+#rm -rf $1"_windows/"/
 
 cd common_files/
 make all
 cd ..
 
-echo Copying to $1"_linux"/
-mkdir $1"_linux"
-cp -r common_files/* $1"_linux"/
-rm -r $1"_linux"/Makefile
-cp -r linux_files/* $1"_linux"/
-rm -r $1"_linux"/src/2DGame/
-cp -r games_src/$1/* $1"_linux"/src/
+echo Copying to output/$1"_linux"/
+mkdir output/$1"_linux"
+cp -r common_files/* output/$1"_linux"/
+rm -r output/$1"_linux"/Makefile
+cp -r linux_files/* output/$1"_linux"/
+rm -r output/$1"_linux"/src/2DGame/
+cp -r games_src/$1/* output/$1"_linux"/src/
 
-python create_project_files.py linux $1 $1"_linux"/
+python create_project_files.py linux $1 output/$1"_linux"/
 
-echo Copying to $1"_windows"
-mkdir $1"_windows"
-cp -R common_files/* $1"_windows"/
-rm -r $1"_windows"/Makefile
-cp -R windows_files/* $1"_windows"/
-rm -r $1"_windows"/src/2DGame/
-cp -r games_src/$1/* $1"_windows"/src/
+echo Copying to output/$1"_windows"
+mkdir output/$1"_windows"
+cp -R common_files/* output/$1"_windows"/
+rm -r output/$1"_windows"/Makefile
+cp -R windows_files/* output/$1"_windows"/
+rm -r output/$1"_windows"/src/2DGame/
+cp -r games_src/$1/* output/$1"_windows"/src/
 
-python create_project_files.py windows $1 $1"_windows"/
+python create_project_files.py windows $1 output/$1"_windows"/
 
 #echo Cleaning up $1"_linux"/
 #mv $1"_linux"/Makefile_non_dev $1"_linux"/Makefile
